12f8c76ad6228b17dec944357ceed35e5f4ed8c0
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-04 23:33:24 -0400

    survey

---
+++ survey.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let answers = "";
+rl.question("What's your name? Nicknames are also acceptable ", (answer) => {
+  answers += `Nice! ${answer}`;
+  let ans1 = answers;
+
+  rl.question("What's an activity you like doing? ", (answer) => {
+    answers += `that's interesting! ${answer}`;
+    
+    rl.question("What do you listen to while doing that? ", (answer) => {
+      answers += `woow nicee :) ${answer}`;
+
+      rl.question("Which meal is your favourite (eg. dinner, brunch, etc.) ", (answer) => {
+        answers += `sounds like ${answer}`;
+
+        rl.question("What's your favourite thing to eat for that meal? ", (answer) => {
+          answers += `Sounds Tasty! ${answer}`;
+
+          rl.question("Which sport is your absolute favourite? ", (answer) => {
+            answers += `that's a nice sport! ${answer}`;
+
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer) => {
+              answers += `Wow good answer :P ${answer}`;
+
+              console.log(ans1);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
